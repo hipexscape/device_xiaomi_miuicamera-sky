bd1bade2df89449ff214f2a8373b0f4a0a394541
Alexander Winkowski <dereference23@outlook.com>
2024-01-14 09:21:50 +0100

    sky-miuicamera: Improve the shim
    Change-Id: I415e53e25d0f0ddbc9c1e988d8b65ebdeafe9d6e

--- shims/libgui_shim_miuicamera.c
+++ shims/libgui_shim_miuicamera.c
@@ -4,6 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void _ZN7android18BnProducerListener16onBufferDetachedEi() {
-    return;
+void _ZN7android18BnProducerListener16onBufferDetachedEi(int slot) {
+    (void)slot;
 }
